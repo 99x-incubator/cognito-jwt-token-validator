71394e92a3a396d0a011d7fa5d1249fda56806e6
Pasindu Wijesena <pasinduw@99x.lk>
2017-12-14 14:53:11 +0530

    Added the test agent

---
+++ test/test-agent.ts
@@ -0,0 +1,30 @@
+process.env.DEBUG = 'cognito-jwt-token-validator';
+
+import * as readline from 'readline';
+import {Validator} from '../src';
+
+
+async function testToken(iss: string, aud: string, rl: readline.ReadLine) {
+  return new Promise<string>(resolve => {
+           rl.question('Token: ', (token) => {
+             resolve(token);
+           });
+         })
+      .then((token) => {
+        const validator = new Validator(iss, aud);
+        return validator.validate(token);
+      });
+}
+
+
+
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.question('Issuer: ', (iss) => {
+  rl.question('Audience: ', (aud) => {
+    testToken(iss, aud, rl)
+        .then((res) => console.log(res))
+        .then(()=> rl.close())
+        .catch((err) => console.log(err));
+  });
+});

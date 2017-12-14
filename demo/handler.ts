1d6c8f187afbba41e5bc1de6d7f9294e154822d8
Pasindu Wijesena <pasinduw@99x.lk>
2017-12-14 16:03:18 +0530

    demo serverless code

---
+++ demo/handler.ts
@@ -0,0 +1,52 @@
+import { Validator } from '../build/src/index';
+
+const policyMap = require('./policy-map.json');
+
+export const authorize = async (event, context, cb) => {
+  const token = event.authorizationToken;
+  const validator = new Validator(process.env.iss, process.env.aud, false);
+
+  if (!token) {
+    return cb('Missing authorization token');
+  }
+
+  try {
+    const {sub} = await validator.validate(token);
+
+    const policyDocument = {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: 'allow',
+          Resource: '*'
+        }
+      ]
+    };
+
+    const response = {
+      principalId: sub,
+      policyDocument
+    };
+
+    console.log('Authorization successful. Sending policy document: ');
+    console.log(JSON.stringify(response, null, 2));
+    cb(null, response);
+  } catch (err) {
+    console.log('An error occurred while authorizing: ');
+    console.log(err);
+    cb(err);
+  }
+};
+
+export const hello = (event, context, cb) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  cb(null, response);
+}
